74e2c43e695ce3e4cd42ec7371fd47073b0a4ce9
John Davis <johnsdavis95@gmail.com>
2017-06-25 17:52:27 -0400

    almost finished with manager

--- bieBayManager.js
+++ bieBayManager.js
@@ -2,6 +2,10 @@ var mysql = require('mysql');
 
 var inquirer = require('inquirer');
 
+var items = [];
+
+var itemUpdated;
+
 var connection = mysql.createConnection({
 	
 	host: 'localhost',
@@ -14,6 +18,9 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
 	if (err) throw err;
 
+	runManager();
+
+function runManager() {
 	inquirer.prompt([
 			{
 				type: 'list',
@@ -36,22 +43,130 @@ connection.connect(function(err) {
 					break;
 
 				case 'Add New Product':
-					addProduct();
+					addNewProduct();
 					break;
 
-				default: "Invalid Coice";
+				default: 
+					console.log("Invalid Choice");
 
 
 			}
 		})
-	
+}	
 
 		function viewProducts() {
+			
 			connection.query("SELECT `item_id`, `product_name`, `price`, `stock_quantity` FROM `products`", function(err, data) {
+				
 				if (err) throw err;
 
-				console.log(data);
+				for (var i = 0; i < data.length; i++) {
+					
+					console.log("ID:",  data[i].item_id, "PRODUCT:", data[i].product_name, "PRICE: $" + data[i].price, "STOCK QUANTITY:", data[i].stock_quantity);
+				}
+				
 			})
+		}
+
+		function viewLow() {
+			
+			connection.query("SELECT `item_id`, `product_name`, `price`, `stock_quantity` FROM `products` WHERE `stock_quantity` < 12", function(err, data) {
+
+				if (err) throw err;
+
+				for (var i = 0; i < data.length; i++) {
+
+					console.log("ID:",  data[i].item_id, "PRODUCT:", data[i].product_name, "PRICE: $" + data[i].price, "STOCK QUANTITY:", data[i].stock_quantity);
+
+				}
+			})	
+		}
+
+		function addInventory() {
+
+			connection.query("SELECT `product_name` FROM `products`", function(err, data) {
+
+				for (var i = 0; i < data.length; i++) {
+
+					items.push(data[i].product_name);
+
+					// console.log(items);
+				}
+			
+				inquirer.prompt([
+						{
+							type: 'list',
+							message: 'What product would you like to update the inventory on?',
+							name: 'updated',
+							choices: items
+						}
+					]).then(function(response) {
+
+						itemUpdated = response.updated;
+
+						inquirer.prompt([
+								{
+									type: 'input',
+									message: 'Add amount of inventory being added',
+									name: ''
+								}
+							])
+						connection.query("UPDATE `products` SET `stock_quantity` WHERE `product_name` = ?", [itemUpdated], function(err, data) {
+
+							if (err) throw err;
+
+							console.log("Updated stock inventory.");
+						})
+					
+					})
+				
+				
+			})
+
+		}
+
+		function addNewProduct() {
+
+			inquirer.prompt([
+					{
+						type: 'input',
+						message: 'Product Name:',
+						name: 'productName'
+					},
+
+					{
+						type: 'input',
+						message: 'Department Name:',
+						name: 'departmentName'
+					},
+
+					{
+						type: 'input',
+						message: 'Price:',
+						name: 'price'
+					},
+
+					{
+						type: 'input',
+						message: 'Stock Quantity:',
+						name: 'stock'
+					},
+
+					{
+						type: 'input',
+						message: 'Autographed:',
+						name: 'sign'
+					}
+				]).then(function(response) {
+
+					connection.query("INSERT INTO `products` (`product_name`, `department_name`, `price`, `stock_quantity`, `autographed`) VALUES (?, ?, ?, ?, ?)", [response.productName, response.departmentName, response.price, response.stock, response.sign], function(err, data) {
+
+						if (err) throw err;
+
+						console.log("Updated Database");
+					})
+				})
+				
 		}
 
 
